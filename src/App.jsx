43147182b2eae33095d2f1e8e64f37cb675ca69a
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-03-20 07:03:09 +0600

    Axios, Data Extraction and data Processing BarChart

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import LineChart from './Components/LineChart/LineChart'
 import Navbar from './Components/Navbar/Navbar'
+import Phones from './Components/Phones/Phones'
 import PriceOptions from './Components/PriceOptions/PriceOptions'
 
 
@@ -14,6 +15,7 @@ function App() {
       <h1 className='text-3xl bg-red-400'>Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
 
       
 
---
+++ src/Components/Phones/Phones.jsx
@@ -0,0 +1,42 @@
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+const Phones = () => {
+    const [phones, setPhones] = useState([])
+    useEffect(() => {
+        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        // .then(res => res.json())
+        // .then(data => setPhones(data.data))
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => {
+                const phoneData = data.data.data;
+                const phonesWithFakeData = phoneData.map(phone => {
+                    const obj = {
+                        name: phone.slug.phone_name,
+                        price: parseInt(phone.slug.split('-')[1])
+                    }
+                    return obj;
+                })
+                setPhones(phonesWithFakeData)
+            })
+
+    }, [])
+    return (
+        <div className="container mx-auto">
+            <h2 className="text-2xl"> phones:{phones.length}</h2>
+            <BarChart width={800} height={400} data={phones}>
+
+                <Bar dataKey="price" fill="#8884d8" />
+                <XAxis dataKey="name"></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
+
+        </div>
+    );
+};
+
+export default Phones;
